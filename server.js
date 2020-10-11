e925ae847e80970dea7dbc6704574c91eb088f93
Thiago Melo <thiagomelo@MacBook-Pro-de-Thiago.local>
2020-10-11 12:17:19 -0300

    add rotas de exemplo

--- server.js
+++ server.js
@@ -6,6 +6,20 @@ server.get("/", function(requisicao, resposta){
     return resposta.json({"response":process.env.TEXT})
 })
 
+
+server.get("/user/", function(requisicao, resposta){
+    return resposta.json({"response":"você está na rota /user/"})
+})
+
+server.get("/user/:id", function(requisicao, resposta){
+    return resposta.json({"response":"você está na rota/user/:id o id digitado foi: "+ requisicao.params.id})
+})
+
+server.get("/*", function(requisicao, resposta){
+    return resposta.json({"response":"rota não existe"})
+})
+
+
 server.listen(process.env.PORT, function(){
     
     console.log('server is runnig na porta: ' + process.env.PORT)
